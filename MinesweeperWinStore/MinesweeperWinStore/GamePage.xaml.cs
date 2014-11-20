530ef268c9436f918c84bd70181a80b91fdec4f6
garrettxspk <gkelley@harding.edu>
2014-11-19 22:30:50 -0600

    Expanded functionality of reveal mines on loss
    Mines that were properly flagged are not revealed, and cells that were
    flagged, but were not mines indicate that on loss.

--- MinesweeperWinStore/MinesweeperWinStore/GamePage.xaml.cs
+++ MinesweeperWinStore/MinesweeperWinStore/GamePage.xaml.cs
@@ -586,14 +586,26 @@ namespace MinesweeperWinStore
 
         private void revealMines()
         {
-            string newSource = "ms-appx:///images/mine.jpg";
+            string isAMineSource = "ms-appx:///images/mine.jpg";
+            string isNotAMineSource = "ms-appx:///images/falseMine.jpg";
             for (int r = 0; r < gameBoardHeight; r++)
                 for (int c = 0; c < gameBoardWidth; c++)
                 {
                     if (gameBoard[r, c].CellType == MINE)
                     {
-                        Image thisCellImage = gameBoardGrid.Children[r * gameBoardWidth + c] as Image;
-                        (thisCellImage).Source = new BitmapImage(new Uri(newSource, UriKind.Absolute));
+                        if (gameBoard[r, c].State != Cell.CellState.Flagged)
+                        {
+                            Image thisCellImage = gameBoardGrid.Children[r * gameBoardWidth + c] as Image;
+                            (thisCellImage).Source = new BitmapImage(new Uri(isAMineSource, UriKind.Absolute));
+                        }
+                    }
+                    else
+                    {
+                        if (gameBoard[r, c].State == Cell.CellState.Flagged)
+                        {
+                            Image thisCellImage = gameBoardGrid.Children[r * gameBoardWidth + c] as Image;
+                            (thisCellImage).Source = new BitmapImage(new Uri(isNotAMineSource, UriKind.Absolute));
+                        }
                     }
                 }
         }
