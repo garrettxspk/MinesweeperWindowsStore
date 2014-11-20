fd6e618e8703dffcaaaf58f30f873dca66faa71d
garrettxspk <gkelley@harding.edu>
2014-11-20 13:57:17 -0600

    Add Page and Class for HighScores

---
+++ MinesweeperWinStore/MinesweeperWinStore/HighScore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperWinStore
+{
+    class HighScore
+    {
+        private string initials;
+
+        public string Initials
+        {
+            get { return initials; }
+            set 
+            { 
+                if(value.Length > 3)
+                {
+                    initials = value.Substring(0, 3);
+                }
+                initials = initials.ToUpper();
+            }
+        }
+
+        private int score;
+
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+    }
+}
